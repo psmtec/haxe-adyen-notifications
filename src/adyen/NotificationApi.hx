9b6edf350c380212d9111b2841e9923fcfa86ed5
David Klein <dave@smokinheads.de>
2017-03-17 13:48:33 +0100

    remove Parser

--- src/adyen/NotificationApi.hx
+++ src/adyen/NotificationApi.hx
@@ -7,5 +7,4 @@ typedef Error = adyen.notification.Error;
 typedef EventCode = adyen.notification.EventCode;
 typedef Notification = adyen.notification.Notification;
 typedef Operation = adyen.notification.Operation;
-typedef Parser = adyen.notification.Parser;
 typedef RequestItem = adyen.notification.RequestItem;
