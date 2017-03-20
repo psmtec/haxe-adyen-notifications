5ca722a46cc475e34a25fa336836f7f78d00ca91
David Klein <dave@smokinheads.de>
2017-03-20 14:29:21 +0100

    implicit casts for Currency try to parse something with tink.json

--- src/adyen/notification/Currency.hx
+++ src/adyen/notification/Currency.hx
@@ -1,6 +1,6 @@
 package adyen.notification;
 
-@:enum abstract Currency(String) {
+@:enum abstract Currency(String) from String to String {
     var EUR = 'EUR';
     var SEK = 'SEK';
 }
--- test/Main.hx
+++ test/Main.hx
@@ -1,6 +1,11 @@
 using adyen.NotificationApi;
 
+// TODO (DK) add proper tests? for now just see if tink_json can parse it
 class Main {
     public static function main() {
+        try {
+            var n: Notification = tink.Json.parse('{}');
+        } catch (x: Dynamic) {
+        }
     }
 }
