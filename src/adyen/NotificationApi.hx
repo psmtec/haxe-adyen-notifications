ccc3a3c8985ca5df2b6c51dde53d69f415d8c690
David Klein <dave@smokinheads.de>
2017-03-20 14:34:44 +0100

    export all types in api wrapper

--- src/adyen/NotificationApi.hx
+++ src/adyen/NotificationApi.hx
@@ -6,8 +6,22 @@ typedef Amount = adyen.notification.Amount;
 typedef Currency = adyen.notification.Currency;
 typedef Error = adyen.notification.Error;
 typedef EventCode = adyen.notification.EventCode;
-typedef EventDate = adyen.notification.Types.EventDate;
 typedef Notification = adyen.notification.Notification;
 typedef Operation = adyen.notification.Operation;
 typedef RequestItem = adyen.notification.RequestItem;
-// import adyen.notification.Types;
+
+typedef ShopperReference = adyen.notification.AdditionalData.ShopperReference;
+typedef ShopperEmail = adyen.notification.AdditionalData.ShopperEmail;
+typedef ShopperIP = adyen.notification.AdditionalData.ShopperIP;
+typedef AuthCode = adyen.notification.AdditionalData.AuthCode;
+typedef CardSummary = adyen.notification.AdditionalData.CardSummary;
+typedef CardExpiryDate = adyen.notification.AdditionalData.CardExpiryDate;
+typedef AuthorisedAmountValue = adyen.notification.AdditionalData.AuthorisedAmountValue;
+
+typedef EventDate = adyen.notification.Types.EventDate;
+typedef MerchantAccountCode = adyen.notification.Types.MerchantAccountCode;
+typedef MerchantReference = adyen.notification.Types.MerchantReference;
+typedef OriginalReference = adyen.notification.Types.OriginalReference;
+typedef PaymentMethod = adyen.notification.Types.PaymentMethod;
+typedef PSPReference = adyen.notification.Types.PSPReference;
+typedef Reason = adyen.notification.Types.Reason;
